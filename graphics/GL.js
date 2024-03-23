7ebf5801bb3f2a1990c99bc16048d920bb9a22e4
KR-jaju <wnwoals0127@gmail.com>
2024-03-23 14:48:19 +0900

    Refactor: refactor gl context

--- graphics/GL.js
+++ graphics/GL.js
@@ -1,8 +1,7 @@
 
-let gl_canvas = null;
 let	gl_context = null;
 
-function initGL(canvas){
-	gl_canvas = canvas;
-	gl_context = gl_canvas.getContext("webgl2");
+function glMakeCurrent(gl){
+	gl_context = gl;
 }
+
